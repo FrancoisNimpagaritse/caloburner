64d8ef5a214be8ab685a798715773539d2af5e67
François Nimpagaritse <franimpa@yahoo.fr>
2022-08-06 11:22:05 +0200

    Font and image redo

--- caloburner/app/admin.py
+++ caloburner/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Metric
+
+
+# Configure the model for display fields
+class MetricAdmin(admin.ModelAdmin):
+    list_display = ('sport_day', 'distance_done')
+
 
 # Register your models here.
+admin.site.register(Metric)
--- caloburner/app/models.py
+++ caloburner/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Metric(models.Model):
+    sport_day = models.DateTimeField(auto_now_add=True)
+    distance_done = models.FloatField()
+
+    def __str__(self):
+        return str(self.sport_day) + ' -> ' + str(self.sport_hour)
